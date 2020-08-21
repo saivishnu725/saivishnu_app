985dddd991eac070892e0135ff201251f6a184c5
Sai vishnu <saivishnu725@gmail.com>
2020-08-21 14:09:13 +0530

    Added AlertBox and a RaisedButton in home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import './screens/home.dart ';
 void main()
-{  debugProfileBuildsEnabled :false;
+{
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Exploring ',
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -8,11 +9,13 @@ class Home extends StatelessWidget {
     return Center(
         child: Container(
             alignment: Alignment.centerLeft,
-            color: Colors.black45,
-            padding: EdgeInsets.only(left: 10.0, top: 370.0, right: 10.0),
+            color: Colors.blueAccent,
+            padding: EdgeInsets.only(left: 15.0, top: 30.0, right: 5.0), //top:370.0 for centre
             child: Column(children: [
               column1(),
               column2(),
+              ImageOfCat(),
+              button()
             ])
         )
     );
@@ -84,13 +87,53 @@ class column2 extends StatelessWidget {
                       decoration: TextDecoration.none,
                       fontFamily: 'Comic Sans MS',
                       fontWeight: FontWeight.w700,
-                      fontSize: 35.0))),
-          Expanded(child: ImageOfCat())
+                      fontSize: 35.0)))
         ],
       ),
-
-
     );
   }
+}
 
+class button extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return  Container(
+      margin: EdgeInsets.all(15.0),
+      width: 150.0,
+      height: 50.0,
+      child: RaisedButton(
+        color: Colors.red,
+        elevation: 16.0,
+        child: Text(
+          "Click for a alert",
+          style: TextStyle(
+            fontSize: 16.0,
+            color: Colors.yellow,
+            fontFamily: 'Comic Sans MS',
+          ),
+        ),
+        onPressed: ()
+        {
+          popupbox(context);
+        }
+      ),
+    );
+  }
+  void popupbox(BuildContext context)
+  {
+    var alert = AlertDialog(
+      title: Text("About that cat"),
+      content: Text("That cat is sad but accepting what others are saying "),
+
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context)
+        {
+          return alert;
+        }
+    );
+  }
 }
