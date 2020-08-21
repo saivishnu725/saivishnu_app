42253b6ed6c6078fd645ce0dd4de5bcf7a9e5465
Sai vishnu <saivishnu725@gmail.com>
2020-08-21 14:19:26 +0530

    Indented the code

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,15 +10,10 @@ class Home extends StatelessWidget {
         child: Container(
             alignment: Alignment.centerLeft,
             color: Colors.blueAccent,
-            padding: EdgeInsets.only(left: 15.0, top: 30.0, right: 5.0), //top:370.0 for centre
-            child: Column(children: [
-              column1(),
-              column2(),
-              ImageOfCat(),
-              button()
-            ])
-        )
-    );
+            padding: EdgeInsets.only(left: 15.0, top: 30.0, right: 5.0),
+            //top:370.0 for centre
+            child: Column(
+                children: [column1(), column2(), ImageOfCat(), button()])));
   }
 }
 
@@ -26,13 +21,18 @@ class ImageOfCat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AssetImage img = AssetImage('images/cat_sad.png');
-    Image image = Image(image: img, width: 250.0, height: 150.0,);
-    return Container(child: image,);
+    Image image = Image(
+      image: img,
+      width: 250.0,
+      height: 150.0,
+    );
+    return Container(
+      child: image,
+    );
   }
 }
 
-class column1 extends StatelessWidget
-{
+class column1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -58,7 +58,6 @@ class column1 extends StatelessWidget
       ],
     );
   }
-
 }
 
 class column2 extends StatelessWidget {
@@ -94,46 +93,39 @@ class column2 extends StatelessWidget {
   }
 }
 
-class button extends StatelessWidget
-{
+class button extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
-    return  Container(
+  Widget build(BuildContext context) {
+    return Container(
       margin: EdgeInsets.all(15.0),
       width: 150.0,
       height: 50.0,
       child: RaisedButton(
-        color: Colors.red,
-        elevation: 16.0,
-        child: Text(
-          "Click for a alert",
-          style: TextStyle(
-            fontSize: 16.0,
-            color: Colors.yellow,
-            fontFamily: 'Comic Sans MS',
+          color: Colors.red,
+          elevation: 16.0,
+          child: Text(
+            "Click for a alert",
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.yellow,
+              fontFamily: 'Comic Sans MS',
+            ),
           ),
-        ),
-        onPressed: ()
-        {
-          popupbox(context);
-        }
-      ),
+          onPressed: () {
+            popupbox(context);
+          }),
     );
   }
-  void popupbox(BuildContext context)
-  {
+
+  void popupbox(BuildContext context) {
     var alert = AlertDialog(
       title: Text("About that cat"),
       content: Text("That cat is sad but accepting what others are saying "),
-
     );
     showDialog(
-      context: context,
-      builder: (BuildContext context)
-        {
+        context: context,
+        builder: (BuildContext context) {
           return alert;
-        }
-    );
+        });
   }
 }
